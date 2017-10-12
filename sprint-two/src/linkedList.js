087ba31a314cb47c211f92c5b3c9fe3b7336ad98
kfang1220 <fangk1220@gmail.com>
2017-10-12 11:28:56 -0700

    Completed Linked Lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // [] [] [] [] []
+    //have previous tail point at this new node
+    //have list.tail point at last node
+    var newNode = Node();
+    newNode.value = value;
+    
+    if (list.tail === null) {
+      list.tail = newNode;
+      list.head = newNode;
+    }
+    
+    list.tail.next = newNode;
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var temp = list.head;
+    list.head = list.head.next;
+    return temp.value;
   };
 
   list.contains = function(target) {
+    //contains should return a boolean if target is within or not within
+    var current = list.head;
+    var bool = false;
+    while (current !== null) {
+      if (target === current.value) {
+        bool = true;
+      }
+      current = current.next;
+    }
+    return bool;
   };
 
   return list;
