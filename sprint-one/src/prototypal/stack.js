516b5c7180582d064c6498918367bacc8625643c
kfang1220 <fangk1220@gmail.com>
2017-10-11 17:37:34 -0700

    Stuck on Prototype

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(Stack.prototype);
+  someInstance.count = 0;
+  someInstance.container = {};
+  
+  //_.extend(someInstance, stackMethods);
+  
+  return someInstance;
 };
 
 var stackMethods = {};
 
+Stack.prototype.size = function() {
+  return this.count;
+};
+
+Stack.prototype.push = function(value) {
+  this.container[this.count] = value;
+  this.count++;
+};
+
+Stack.prototype.pop = function() {
+  if (this.count !== 0) {
+    this.count--;
+  }
+  return this.container[this.count];
+};
+
+var newstack = Stack;
 
