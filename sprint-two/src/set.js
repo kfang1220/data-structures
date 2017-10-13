0cc02e31864fd3aacc604e9fe6e250cdcf8e870b
kevinfang <fangk1220@gmail.com>
2017-10-12 23:48:57 -0700

    Completed Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+	this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+	for (var key in this._storage) {
+    if (key === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+	delete this._storage[item];
 };
 
 /*
