834d69d4540274c9afe1b44685d5b516bcbd396d
kfang1220 <fangk1220@gmail.com>
2017-10-12 17:16:52 -0700

    Completed Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,14 @@
 var BinarySearchTree = function(value) {
+  var binarySearchTree = {};
 };
 
-
+ 
+BinarySearchTree.prototype.insert = function () {
+};
+BinarySearchTree.prototype.contains = function () {
+};
+BinarySearchTree.prototype.depthFirstLog = function () {
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,67 @@
 var Tree = function(value) {
   var newTree = {};
-  newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.value = value || null;
+  newTree.children = []; // wants an array of children that have new tree properties
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //child should receive a value
+  //should add children to the tree
+  //create a new child object
+  var child = Tree(value);
+  
+  this.children.push(child); // currently applying the parent with child values
 };
 
 treeMethods.contains = function(target) {
+  //should return a boolean for a value in a tree
+  var bool = false;
+  
+  if (this.value === target) {
+    bool = true;
+  }
+  
+  
+  for (var i = 0; i < this.children.length; i++) {
+    //var recursiveBool = this.contains(this.children[i]);
+    if (this.children[i].contains(target)) {
+      //this.children[i].contains(target)  --looks better
+      bool = true;
+    }
+  }
+
+  return bool;
 };
 
 
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? O( n )
  */
+/*
+  var familyTree = {
+  someValueName: #,
+  someParentNameOrValue: #,
+  ifTheyHaveChildren: [
+    childOne = {
+      someValueName: #,
+      someParentNameOrValue: #,
+      ifTheyHaveChildren: []
+    }, 
+    childTwo = {
+      someValueName: #,
+      someParentNameOrValue: #,
+      ifTheyHaveChildren: []
+  
+    },   
+    childThree = {
+      someValueName: #,
+      someParentNameOrValue: #,
+      ifTheyHaveChildren: []
+    }]
+  }
+*/
