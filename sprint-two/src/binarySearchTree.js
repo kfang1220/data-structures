3f18f6be6b5578237b76a055ca88d14d60a89e4d
kfang1220 <fangk1220@gmail.com>
2017-10-14 10:01:07 -0700

    Completed Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,29 +1,72 @@
 var BinarySearchTree = function(value) {
   //psuedoclassical creates a variable to begin with
-  //var this = Object.create(BinarySearchTree.prototype);
-  //var binarySearchTree = {};
-  //have a next reference
-  //store data as an array. If the key is called upon can return all data
-  //psuedoclassical will also return 'this' for us when invoked
+  var bst = Object.create(BinarySearchTree.prototype);
+  bst.value = value;
+  bst.left = null;
+  bst.right = null;
+  return bst;
 };
 
-BinarySearchTree.prototype.insert = function () {
+BinarySearchTree.prototype.insert = function (value) {
   //calculate if it needs to go left or right of value
   //check if the value is in the right place or continue down
   //once there are no conflicts
+  var newNode = new BinarySearchTree(value);
+  var currentNode = this;
+  //check node.value to the currentNode.value
+  //look at the correct branch and determine if it needs to go further
+  if(newNode.value < currentNode.value) {
+    if(currentNode.left === null) {
+      currentNode.left = newNode;
+    } else {
+      currentNode.left.insert(newNode.value)
+    }
+  } else {
+    if(currentNode.right === null) {
+      currentNode.right = newNode;
+    } else {
+      currentNode.right.insert(newNode.value);
+    }
+  }
+
+  return newNode.value;
 };
-BinarySearchTree.prototype.contains = function () {
+
+BinarySearchTree.prototype.contains = function (value) {
   //determine if the value exists within the tree
   //give back a boolean answer
-};
-BinarySearchTree.prototype.depthFirstLog = function () {
-  //depth first means tracking the first branch as deep as it goes
-  //-before moving onto the next branch
+  var currentNode = this;
+  if(currentNode.value === value) {
+    return true;
+  }
 
-  //needs to
-  //should have a variable to store the current path down the
-  //tree and save the path in case it needs to be returned
-  //start the down the fist branch
+  if(value < currentNode.value) {
+    if(currentNode.left) {
+      if(currentNode.left.contains(value)){
+        return true;
+      }
+    }
+  } else {
+    if(currentNode.right) {
+      if(currentNode.right.contains(value)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+BinarySearchTree.prototype.depthFirstLog = function (callback) {
+  var currentNode = this;
+
+  if(currentNode !== null) {
+    callback(this.value);
+  }
+  if(currentNode.left !== null) {
+    currentNode.left.depthFirstLog(callback);
+  }
+  if(currentNode.right !== null) {
+    currentNode.right.depthFirstLog(callback);
+  }
 };
 /*
  * Complexity: What is the time complexity of the above functions?
